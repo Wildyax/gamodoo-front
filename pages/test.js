dddc0c83a2cea6967a48ba9d5443da16ba511483
Alex <alexom003@hotmail.fr>
2025-11-06 15:51:58 +0100

    First commit

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function Test() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/test") 
+      .then(res => res.json())
+      .then(json => setData(json))
+      .catch(err => console.error("Erreur fetch :", err));
+  }, []);
+
+  if (!data) return <p>Loading...</p>;
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
