bba81e6948ea226838641a2b3fb2a36f02de1383
Koruji <jadecottin212@gmail.com>
2025-11-24 10:20:57 +0100

    feat: add 3 fonts + pictures

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Pixelify_Sans, Jersey_15, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const pixelifySans = Pixelify_Sans({
+    subsets: ["latin"],
+    variable: "--font-pixelify-sans",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const jersey15 = Jersey_15({
+    weight: "400",
+    subsets: ["latin"],
+    variable: "--font-jersey-15",
+});
+
+const sourceCodePro = Source_Code_Pro({
+    subsets: ["latin"],
+    variable: "--font-source-code-pro",
 });
 
 export const metadata: Metadata = {
@@ -23,9 +29,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pixelifySans.variable} ${jersey15.variable} ${sourceCodePro.variable} antialiased`}
       >
         {children}
       </body>
