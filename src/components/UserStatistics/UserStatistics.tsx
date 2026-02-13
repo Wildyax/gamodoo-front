906905a405c5328b2adde1eecba6f29be2be65fa
Koruji <jadecottin212@gmail.com>
2026-02-13 16:36:05 +0100

    feat: add UserStatistics component to dashboard

---
+++ src/components/UserStatistics/UserStatistics.tsx
@@ -0,0 +1,12 @@
+export default function UserStatistics() {
+    return (
+        <>
+            <div className="col-start-5 col-end-6 row-start-1 row-end-4">
+                salut
+            </div>
+            <div className="col-start-5 col-end-6 row-start-4 row-end-6">
+                salut 2
+            </div>
+        </>
+    );
+}
