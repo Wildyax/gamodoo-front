1193372f23d591774cc147a20b4390051b103d9f
Koruji <jadecottin212@gmail.com>
2026-02-14 19:36:35 +0100

    feat: implement logout link in ProfilPage component

--- app/profil/page.tsx
+++ app/profil/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import { useAuth } from "@/src/context/AuthContext";
+import Link from "next/dist/client/link";
+import translate from "./../../src/locales/fr.json";
+
 export default function ProfilPage() {
+    const { token, logout } = useAuth();
     return (
         <>
-            <p>Account Page</p>
+            <Link
+                        href="/"
+                        className={`
+                            px-3 py-1.5
+                            text-sm font-medium
+                            rounded-md
+                            shadow-md
+                            text-white
+                            flex items-center justify-center
+                            whitespace-nowrap`
+                        }
+                        style={{ background: `var(--gradient-brown)` }}
+                        onClick={logout}
+                    >
+                        {translate.navbar.logout}
+                    </Link>
         </>
     );
 }
